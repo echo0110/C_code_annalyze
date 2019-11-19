8fdfcce9a578c7c1b4559dcd7f996fba27ef564c
niuben <820713556@qq.com>
2019-11-19 16:14:55 +0800

    请黑大神 帮忙看下

--- cn/localhost01/main.py
+++ cn/localhost01/main.py
@@ -26,65 +26,62 @@ import copy   #导入copy模块
 import MySQLdb
 import regex as re
 import datetime
+import fileinput
+import os
 
 def analyzecode():
     log_line = 0
     text2_log_line = 0
     key = 0
-    # log_list = []
-    log_list = [([0] * 5) for i in range(10)]
-    num_list = numpy.zeros((20, 10))
-    file = open(r'D:\4G_code\lteserver\LTE_IPR\lteenodeb\gtpuPacketRelay\egtpu\stack\common\src\egtpu_cdb.c')
+    file = open(r'D:\4G_code\lteserver\LTE_IPR\lteenodeb\gtpuPacketRelay\egtpu\stack\common\src\egtpu_cdb.c', 'r+')
     try:
         text_lines = file.readlines()
         text_lines2 = copy.deepcopy(text_lines)  # 对象拷贝，深拷贝
-        print(type(text_lines), text_lines)
-        # text_lines2[59] = text_lines2[59] + text_lines2[60] + text_lines2[61]
-        # print(text_lines2[59])
-        # num_list[1] = text_lines[59]
-        # print(text_lines2[59])
-        # pattern = re.compile(r'[?<=()](.*?)[?=)]')
-        # str1 = pattern.sub("0",  text_lines2[59])
-        # print(str1)
-
+        # print(type(text_lines), text_lines)
         # for line in text_lines:
         for i, line in enumerate(text_lines):            # log_line = log_line+1
             if u'LOG_MSG' in line:
-                # log_line = file.readline()
                 log_line = i
                 print(log_line)
                 text2_log_line = log_line
-                # print(text_lines2[text2_log_line])
                 while text_lines[log_line].find(";", 0, len(text_lines[log_line]) - 1) == -1:
                         text_lines2[text2_log_line] += text_lines[log_line+1]
                         log_line += 1
                         key += 1
-                # print(type(text_lines2[log_line]), text_lines2[log_line])
-                # print(type(text_lines2[text2_log_line]), text_lines2[text2_log_line])
-                # pattern = re.compile(r'(?<=().+?(?=))')
-                # pattern = re.compile(r'(?<=().+?(?=))')
-                # pattern = re.compile(r'[(](.*?)[)]')
-                # pattern = re.compile(r'[(](.*?)[)]')
-                # str1 = re.sub(r'(?<=().+?(?=))', "0", "adod(23d)das")
-
-                # pattern = re.compile(r'[(](?:.|\n)*?[)]')
-                # # pattern = re.compile(r'[?<=(].+?[?=)]')
-                # # str1 = pattern.sub('(8)',  "log_msg(23d)das", re.DOTALL)
-
                 pattern = re.compile(r'["](?:.|\n)*?["]')
                 log_str = pattern.findall(text_lines2[text2_log_line])
-                print("x=%s\n", log_str)
+                # print("x=%s\n", log_str)
                 sql_id = mysqlconnect(log_str)
                 print("sql_id=%d\n", sql_id)
+                position = file.tell()
+                print("当前文件位置 : ", position)
                 sql_id_to_str = '%d' % sql_id
                 str1 = pattern.sub(sql_id_to_str,  text_lines2[text2_log_line], re.DOTALL)
                 print(str1)
-
-
-                # print(type(text_lines), log_list)
-        # print(type(text_lines), log_list)
+                # file.write(line.replace(str1, text_lines2[text2_log_line]))
     finally:
         file.close()
+
+
+def write_process():
+    # file = open('D:/4G_code/lteserver/LTE_IPR/lteenodeb/gtpuPacketRelay/egtpu/stack/common/src/egtpu_cdb.c', 'w+')
+    file = open(r'D:\4G_code\lteserver\LTE_IPR\lteenodeb\gtpuPacketRelay\egtpu\stack\common\src\egtpu_cdb.c')
+    # txt_file = open(r'C:\Users\casa\Desktop\test.txt', 'w+')
+
+    for line in fileinput.input(r'C:\Users\casa\Desktop\test.txt', inplace=1):
+        if u'Python' in line:
+            i = 0
+            # pattern = re.compile(r'["](?:.|\n)*?["]')
+            # log_str = pattern.findall(line)
+            #
+            # str1 = pattern.sub('8', log_str, re.DOTALL)
+            # file.write(line.replace('8', pattern))
+
+
+
+
+
+
 
 
 
@@ -110,7 +107,7 @@ def mysqlconnect(str):
         print("already commit")
         # print(db.insert_id()) last_id = curs.lastrowid
         last_id = cursor.lastrowid
-        print(last_id)
+        # print(last_id)
         return last_id;
     except Exception as e:
         db.rollback()  # 发生错误时回滚
@@ -122,7 +119,8 @@ if __name__ == '__main__':
     # 正则：解析留言内容
     # re_note = re.compile(
     #     ur"^留言:[\u3000\u0020]*([\w.-]+@[\w.-]+\.\w+)\s*$")  # 格式; 留言： +任意空格+邮箱
-    analyzecode()
+    # analyzecode()
+    write_process()
     # mysqlconnect()
 
 
